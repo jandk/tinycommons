a184127942a8300681cb70aa70339d847a15a927
Jan De Kock <jan@twofold.be>
2020-11-12 15:07:27 +0100

    Add CharPredicate

---
+++ src/main/java/be/twofold/common/text/CharPredicate.java
@@ -0,0 +1,284 @@
+package be.twofold.common.text;
+
+import be.twofold.common.*;
+
+import java.util.*;
+import java.util.function.*;
+
+public abstract class CharPredicate implements Predicate<Character> {
+
+    CharPredicate() {
+    }
+
+    public static CharPredicate any() {
+        return Any.Instance;
+    }
+
+    public static CharPredicate none() {
+        return None.Instance;
+    }
+
+    public static CharPredicate is(char match) {
+        return new Is(match);
+    }
+
+    public static CharPredicate among(CharSequence matches) {
+        switch (matches.length()) {
+            case 0:
+                return none();
+            case 1:
+                return is(matches.charAt(0));
+            default:
+                return new Among(matches);
+        }
+    }
+
+    public static CharPredicate range(char from, char to) {
+        return new Range(from, to);
+    }
+
+    public static CharPredicate from(Predicate<? super Character> predicate) {
+        return predicate instanceof CharPredicate
+            ? (CharPredicate) predicate
+            : new From(predicate);
+    }
+
+    public abstract boolean matches(char c);
+
+    @Override
+    @Deprecated
+    public final boolean test(Character c) {
+        return matches(c);
+    }
+
+    @Override
+    public CharPredicate and(Predicate<? super Character> other) {
+        return and(from(other));
+    }
+
+    @Override
+    public CharPredicate or(Predicate<? super Character> other) {
+        return or(from(other));
+    }
+
+    @Override
+    public CharPredicate negate() {
+        return not();
+    }
+
+    public CharPredicate and(CharPredicate other) {
+        return new And(this, other);
+    }
+
+    public CharPredicate or(CharPredicate other) {
+        return new Or(this, other);
+    }
+
+    public CharPredicate not() {
+        return new Not(this);
+    }
+
+
+    public String stripFrom(CharSequence cs) {
+        int len = cs.length();
+
+        int from;
+        for (from = 0; from < len; from++) {
+            if (!matches(cs.charAt(from))) {
+                break;
+            }
+        }
+
+        int to;
+        for (to = len - 1; to > from; to--) {
+            if (!matches(cs.charAt(to))) {
+                break;
+            }
+        }
+
+        return cs.subSequence(from, to + 1).toString();
+    }
+
+    public String stripLeadingFrom(CharSequence cs) {
+        for (int i = 0, len = cs.length(); i < len; i++) {
+            if (!matches(cs.charAt(i))) {
+                return cs.subSequence(i, len).toString();
+            }
+        }
+        return "";
+    }
+
+    public String stripTrailingFrom(CharSequence cs) {
+        for (int i = cs.length() - 1; i >= 0; i--) {
+            if (!matches(cs.charAt(i))) {
+                return cs.subSequence(0, i + 1).toString();
+            }
+        }
+        return "";
+    }
+
+
+    static final class Any extends CharPredicate {
+        static final Any Instance = new Any();
+
+        private Any() {
+        }
+
+
+        @Override
+        public boolean matches(char c) {
+            return true;
+        }
+
+        @Override
+        public CharPredicate and(CharPredicate other) {
+            return Check.notNull(other);
+        }
+
+        @Override
+        public CharPredicate or(CharPredicate other) {
+            Check.notNull(other);
+            return this;
+        }
+
+        @Override
+        public CharPredicate not() {
+            return CharPredicate.none();
+        }
+    }
+
+    static final class None extends CharPredicate {
+        static final None Instance = new None();
+
+        private None() {
+        }
+
+
+        @Override
+        public boolean matches(char c) {
+            return false;
+        }
+
+        @Override
+        public CharPredicate and(CharPredicate other) {
+            Check.notNull(other);
+            return this;
+        }
+
+        @Override
+        public CharPredicate or(CharPredicate other) {
+            return Check.notNull(other);
+        }
+
+        @Override
+        public CharPredicate not() {
+            return any();
+        }
+    }
+
+    static final class Is extends CharPredicate {
+        private final char match;
+
+        Is(char match) {
+            this.match = match;
+        }
+
+
+        @Override
+        public boolean matches(char c) {
+            return c == match;
+        }
+    }
+
+    static final class Among extends CharPredicate {
+        private final char[] matches;
+
+        Among(CharSequence matches) {
+            this.matches = matches.toString().toCharArray();
+            Arrays.sort(this.matches);
+        }
+
+        @Override
+        public boolean matches(char c) {
+            return Arrays.binarySearch(matches, c) >= 0;
+        }
+    }
+
+    static final class Range extends CharPredicate {
+        private final char from;
+        private final char to;
+
+        Range(char from, char to) {
+            Check.argument(from <= to);
+            this.from = from;
+            this.to = to;
+        }
+
+        @Override
+        public boolean matches(char c) {
+            return c >= from && c <= to;
+        }
+    }
+
+    static final class From extends CharPredicate {
+        private final Predicate<? super Character> predicate;
+
+        From(Predicate<? super Character> predicate) {
+            this.predicate = Check.notNull(predicate);
+        }
+
+        @Override
+        public boolean matches(char c) {
+            return predicate.test(c);
+        }
+    }
+
+    static final class And extends CharPredicate {
+        private final CharPredicate first;
+        private final CharPredicate second;
+
+        And(CharPredicate first, CharPredicate second) {
+            this.first = Check.notNull(first);
+            this.second = Check.notNull(second);
+        }
+
+        @Override
+        public boolean matches(char c) {
+            return first.matches(c) && second.matches(c);
+        }
+    }
+
+    static final class Or extends CharPredicate {
+        private final CharPredicate first;
+        private final CharPredicate second;
+
+        Or(CharPredicate first, CharPredicate second) {
+            this.first = Check.notNull(first);
+            this.second = Check.notNull(second);
+        }
+
+        @Override
+        public boolean matches(char c) {
+            return first.matches(c) || second.matches(c);
+        }
+    }
+
+    static final class Not extends CharPredicate {
+        private final CharPredicate predicate;
+
+        Not(CharPredicate predicate) {
+            this.predicate = Check.notNull(predicate);
+        }
+
+        @Override
+        public boolean matches(char c) {
+            return !predicate.matches(c);
+        }
+
+        @Override
+        public CharPredicate not() {
+            return predicate;
+        }
+    }
+
+}
---
+++ src/test/java/be/twofold/common/text/CharPredicateTest.java
@@ -0,0 +1,219 @@
+package be.twofold.common.text;
+
+import org.junit.*;
+
+import java.util.function.*;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SuppressWarnings("ConstantConditions")
+public class CharPredicateTest {
+    private static final char C1 = 'a';
+    private static final char C2 = 'z';
+
+
+    @Test
+    public void testNegate() {
+        CharPredicate predicate = CharPredicate.is(C1);
+        testAllChars(predicate.negate(), c -> c != C1, "negate");
+    }
+
+    @Test
+    @SuppressWarnings({"deprecation", "FunctionalExpressionCanBeFolded"})
+    public void testTest() {
+        CharPredicate predicate = CharPredicate.is(C1);
+        testAllChars(predicate, predicate::test, "test");
+    }
+
+
+    @Test
+    public void testAny() {
+        CharPredicate predicate = CharPredicate.any();
+
+        testAllChars(predicate, c -> true, "any");
+        assertThat(CharPredicate.any()).isEqualTo(predicate);
+    }
+
+    @Test
+    public void testNone() {
+        CharPredicate predicate = CharPredicate.none();
+
+        testAllChars(predicate, c -> false, "none");
+        assertThat(CharPredicate.none()).isEqualTo(predicate);
+    }
+
+    @Test
+    public void testIs() {
+        CharPredicate predicate = CharPredicate.is(C1);
+
+        testAllChars(predicate, c -> c == C1, "is");
+    }
+
+    @Test
+    public void testRange() {
+        CharPredicate predicate = CharPredicate.range(C1, C2);
+
+        testAllChars(predicate, c -> c >= C1 && c <= C2, "range");
+    }
+
+    @Test
+    public void testAmong() {
+        CharPredicate predicate = CharPredicate.among("abc");
+
+        testAllChars(predicate, c -> c == 'a' || c == 'b' || c == 'c', "among");
+    }
+
+    @Test
+    public void testForPredicate() {
+        CharPredicate predicate = CharPredicate.from(character -> character == 'a');
+        testAllChars(predicate, character -> character == 'a', "from");
+    }
+
+
+    @Test
+    public void testAnd() {
+        CharPredicate p1 = CharPredicate.range('a', 'n');
+        CharPredicate p2 = CharPredicate.range('m', 'z');
+        CharPredicate predicate = p1.and(p2);
+
+        testAllChars(predicate, c -> c == 'm' || c == 'n', "and");
+    }
+
+    @Test
+    public void testOr() {
+        CharPredicate p1 = CharPredicate.range('a', 'n');
+        CharPredicate p2 = CharPredicate.range('m', 'z');
+        CharPredicate predicate = p1.or(p2);
+
+        testAllChars(predicate, c -> c >= 'a' && c <= 'z', "or");
+    }
+
+    @Test
+    public void testNot() {
+        CharPredicate predicate = CharPredicate.range(C1, C2).not();
+
+        testAllChars(predicate, c -> c < C1 || c > C2, "not");
+    }
+
+
+    @Test
+    public void testStripFrom() {
+        CharPredicate predicate = CharPredicate.is(C1);
+
+        assertThat(predicate.stripFrom("aaabbbaaa")).isEqualTo("bbb");
+        assertThat(predicate.stripFrom("bbbaaabbb")).isEqualTo("bbbaaabbb");
+        assertThat(predicate.stripFrom("aaaaaaaaa")).isEqualTo("");
+    }
+
+    @Test
+    public void testStripFromThrowsNPE() {
+        CharPredicate predicate = CharPredicate.is(C1);
+
+        assertThatNullPointerException()
+            .isThrownBy(() -> predicate.stripFrom(null));
+    }
+
+    @Test
+    public void testStripLeadingFrom() {
+        CharPredicate predicate = CharPredicate.is(C1);
+
+        assertThat(predicate.stripLeadingFrom("aaabbbaaa")).isEqualTo("bbbaaa");
+        assertThat(predicate.stripLeadingFrom("bbbaaabbb")).isEqualTo("bbbaaabbb");
+        assertThat(predicate.stripLeadingFrom("aaaaaaaaa")).isEqualTo("");
+    }
+
+    @Test
+    public void testStripLeadingFromThrowsNPE() {
+        CharPredicate predicate = CharPredicate.is(C1);
+
+        assertThatNullPointerException()
+            .isThrownBy(() -> predicate.stripLeadingFrom(null));
+    }
+
+    @Test
+    public void testStripTrailingFrom() {
+        CharPredicate predicate = CharPredicate.is(C1);
+
+        assertThat(predicate.stripTrailingFrom("aaabbbaaa")).isEqualTo("aaabbb");
+        assertThat(predicate.stripTrailingFrom("bbbaaabbb")).isEqualTo("bbbaaabbb");
+        assertThat(predicate.stripTrailingFrom("aaaaaaaaa")).isEqualTo("");
+    }
+
+    @Test
+    public void testStripTrailingFromThrowsNPE() {
+        CharPredicate predicate = CharPredicate.is(C1);
+
+        assertThatNullPointerException()
+            .isThrownBy(() -> predicate.stripTrailingFrom(null));
+    }
+
+
+    @Test
+    public void testAnyOr() {
+        CharPredicate predicate = CharPredicate.any();
+
+        assertThat(predicate.or(CharPredicate.is(C1))).isEqualTo(predicate);
+    }
+
+    @Test
+    public void testAnyAnd() {
+        CharPredicate predicate = CharPredicate.any();
+        CharPredicate other = CharPredicate.is(C1);
+
+        assertThat(predicate.and(other)).isEqualTo(other);
+    }
+
+    @Test
+    public void testAnyNot() {
+        CharPredicate predicate = CharPredicate.any();
+
+        assertThat(predicate.not()).isEqualTo(CharPredicate.none());
+    }
+
+    @Test
+    public void testNoneOr() {
+        CharPredicate predicate = CharPredicate.none();
+        CharPredicate other = CharPredicate.is(C1);
+
+        assertThat(predicate.or(other)).isEqualTo(other);
+    }
+
+    @Test
+    public void testNoneAnd() {
+        CharPredicate predicate = CharPredicate.none();
+
+        assertThat(predicate.and(CharPredicate.is(C1))).isEqualTo(predicate);
+    }
+
+    @Test
+    public void testNoneNot() {
+        CharPredicate predicate = CharPredicate.none();
+
+        assertThat(predicate.not()).isEqualTo(CharPredicate.any());
+    }
+
+    @Test
+    public void testNotNot() {
+        CharPredicate predicate = CharPredicate.range(C1, C2);
+
+        assertThat(predicate.not().not()).isEqualTo(predicate);
+    }
+
+    @Test
+    public void testForWithCharPredicate() {
+        CharPredicate originalPredicate = CharPredicate.any();
+        CharPredicate predicate = CharPredicate.from(originalPredicate);
+
+        assertThat(predicate).isEqualTo(originalPredicate);
+    }
+
+
+    private void testAllChars(CharPredicate actual, Predicate<Character> expected, String name) {
+        for (char c = Character.MIN_VALUE; c < Character.MAX_VALUE; c++) {
+            if (actual.matches(c) != expected.test(c)) {
+                fail(name + " should not match " + c);
+            }
+        }
+    }
+
+}
