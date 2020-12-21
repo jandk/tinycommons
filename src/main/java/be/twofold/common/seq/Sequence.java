0523d231b1f7621d3501189f533ca1ef7feb896f
Jan De Kock <jan@twofold.be>
2020-12-21 22:08:57 +0100

    Add some terminal operations

--- src/main/java/be/twofold/common/seq/Sequence.java
+++ src/main/java/be/twofold/common/seq/Sequence.java
@@ -1,8 +1,10 @@
 package be.twofold.common.seq;
 
+import be.twofold.common.*;
 import be.twofold.common.seq.Sequences.*;
 
 import java.util.*;
+import java.util.function.*;
 import java.util.stream.*;
 
 @FunctionalInterface
@@ -40,10 +42,195 @@ public interface Sequence<T> extends Iterable<T> {
 
     // endregion
 
-    // region Intermediate Methods
+    // region Intermediate Operations
 
     default Sequence<T> onlyOnce() {
         return new OnlyOnceSequence<>(this);
+    }
+
+    // endregion
+
+    // region Terminal Operations
+
+    default boolean all(Predicate<? super T> predicate) {
+        Check.notNull(predicate, "predicate");
+
+        for (T element : this) {
+            if (!predicate.test(element)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    default boolean any() {
+        return iterator().hasNext();
+    }
+
+    default boolean any(Predicate<? super T> predicate) {
+        Check.notNull(predicate, "predicate");
+
+        for (T element : this) {
+            if (predicate.test(element)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    default boolean none() {
+        return !iterator().hasNext();
+    }
+
+    default boolean none(Predicate<? super T> predicate) {
+        Check.notNull(predicate, "predicate");
+
+        for (T element : this) {
+            if (predicate.test(element)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    default int count() {
+        int count = 0;
+        for (T ignored : this) {
+            count++;
+        }
+        return count;
+    }
+
+    default int count(Predicate<? super T> predicate) {
+        Check.notNull(predicate, "predicate");
+
+        int count = 0;
+        for (T element : this) {
+            if (predicate.test(element)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+
+    default T first() {
+        Iterator<T> iterator = iterator();
+        if (!iterator.hasNext()) {
+            throw new NoSuchElementException("Empty sequence");
+        }
+        return iterator.next();
+    }
+
+    default Optional<T> firstOptional() {
+        Iterator<T> iterator = iterator();
+        if (!iterator.hasNext()) {
+            return Optional.empty();
+        }
+        return Optional.of(iterator.next());
+    }
+
+    default T first(Predicate<? super T> predicate) {
+        Check.notNull(predicate, "predicate");
+
+        for (T element : this) {
+            if (predicate.test(element)) {
+                return element;
+            }
+        }
+        throw new NoSuchElementException("No matching element in sequence");
+    }
+
+    default Optional<T> firstOptional(Predicate<? super T> predicate) {
+        Check.notNull(predicate, "predicate");
+
+        for (T element : this) {
+            if (predicate.test(element)) {
+                return Optional.of(element);
+            }
+        }
+        return Optional.empty();
+    }
+
+
+    default void forEach(Consumer<? super T> consumer) {
+        Check.notNull(consumer, "consumer");
+
+        for (T element : this) {
+            consumer.accept(element);
+        }
+    }
+
+
+    default T reduce(BinaryOperator<T> operation) {
+        Check.notNull(operation, "operation");
+
+        Iterator<T> iterator = iterator();
+        if (!iterator.hasNext()) {
+            throw new NoSuchElementException("Empty sequence");
+        }
+
+        T result = iterator.next();
+        while (iterator.hasNext()) {
+            result = operation.apply(result, iterator.next());
+        }
+        return result;
+    }
+
+    default <R> R reduce(R identity, BiFunction<R, ? super T, R> operation) {
+        Check.notNull(operation, "operation");
+
+        R result = identity;
+        for (T element : this) {
+            result = operation.apply(result, element);
+        }
+        return result;
+    }
+
+
+    default <C extends Collection<? super T>> C toCollection(C collection) {
+        Check.notNull(collection, "collection");
+
+        for (T element : this) {
+            collection.add(element);
+        }
+        return collection;
+    }
+
+    default List<T> toList() {
+        return toCollection(new ArrayList<>());
+    }
+
+    default List<T> toUnmodifiableList() {
+        ArrayList<T> list = toCollection(new ArrayList<>());
+        switch (list.size()) {
+            case 0:
+                return Collections.emptyList();
+            case 1:
+                return Collections.singletonList(list.get(0));
+            default:
+                list.trimToSize();
+                return Collections.unmodifiableList(list);
+        }
+    }
+
+    default Set<T> toSet() {
+        return toCollection(new HashSet<>());
+    }
+
+    default Set<T> toUnmodifiableSet() {
+        HashSet<T> set = toCollection(new HashSet<>());
+        switch (set.size()) {
+            case 0:
+                return Collections.emptySet();
+            case 1:
+                return Collections.singleton(set.iterator().next());
+            default:
+                return Collections.unmodifiableSet(set);
+        }
     }
 
     // endregion
