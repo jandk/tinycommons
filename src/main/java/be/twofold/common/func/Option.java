fe145d5b8810d937d8382582cb82dff3c4699c02
Jan De Kock <jan@twofold.be>
2021-01-02 21:05:18 +0100

    Skeleton implementations for Either, Option and Try

---
+++ src/main/java/be/twofold/common/func/Option.java
@@ -0,0 +1,89 @@
+package be.twofold.common.func;
+
+import java.util.*;
+
+public abstract class Option<T> {
+
+    Option() {
+    }
+
+    public static <T> Option<T> some(T value) {
+        return new Some<>(value);
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T> Option<T> none() {
+        return (Option<T>) None.Instance;
+    }
+
+    public static <T> Option<T> of(T value) {
+        return value != null ? some(value) : none();
+    }
+
+    public abstract T get();
+
+    public boolean isPresent() {
+        return this instanceof Some;
+    }
+
+    public boolean isAbsent() {
+        return this instanceof None;
+    }
+
+    static final class Some<T> extends Option<T> {
+        private final T value;
+
+        Some(T value) {
+            this.value = value;
+        }
+
+        @Override
+        public T get() {
+            return value;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            return this == obj || obj instanceof Some
+                && Objects.equals(value, ((Some<?>) obj).value);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hashCode(value);
+        }
+
+        @Override
+        public String toString() {
+            return "Some(" + value + ")";
+        }
+    }
+
+    static final class None extends Option<Void> {
+        static final None Instance = new None();
+
+        private None() {
+        }
+
+        @Override
+        public Void get() {
+            throw new NoSuchElementException();
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            return this == obj || obj instanceof Some;
+        }
+
+        @Override
+        public int hashCode() {
+            return 1;
+        }
+
+        @Override
+        public String toString() {
+            return "None()";
+        }
+    }
+
+}
---
+++ src/main/java/be/twofold/common/func/Try.java
@@ -0,0 +1,78 @@
+package be.twofold.common.func;
+
+import java.util.*;
+
+public abstract class Try<T> {
+
+    Try() {
+    }
+
+    public static <T> Try<T> success(T result) {
+        return new Success<>(result);
+    }
+
+    public static <T> Failure<T> failure(Throwable cause) {
+        return new Failure<>(cause);
+    }
+
+    public boolean isSuccess() {
+        return this instanceof Success;
+    }
+
+    public boolean isFailure() {
+        return this instanceof Failure;
+    }
+
+    static final class Success<T> extends Try<T> {
+        private final T result;
+
+        Success(T result) {
+            this.result = result;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            return this == obj || obj instanceof Success
+                && Objects.equals(result, ((Success<?>) obj).result);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hashCode(result);
+        }
+
+        @Override
+        public String toString() {
+            return "Success(" + result + ")";
+        }
+    }
+
+    static final class Failure<T> extends Try<T> {
+        private final Throwable cause;
+
+        Failure(Throwable cause) {
+            this.cause = cause;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof Failure)) return false;
+
+            Failure<?> failure = (Failure<?>) o;
+
+            return Objects.equals(cause, failure.cause);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hashCode(cause);
+        }
+
+        @Override
+        public String toString() {
+            return "Failure(" + cause + ")";
+        }
+    }
+
+}
