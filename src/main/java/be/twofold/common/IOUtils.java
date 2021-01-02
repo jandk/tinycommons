6640b4bbd90a10033b0a3b1e6fc622a9bad9834d
Jan De Kock <jan@twofold.be>
2021-01-02 22:45:42 +0100

    Add some more useful methods

--- src/main/java/be/twofold/common/IOUtils.java
+++ src/main/java/be/twofold/common/IOUtils.java
@@ -4,6 +4,8 @@ import java.io.*;
 
 public final class IOUtils {
 
+    private static final int BufferSize = 8 * 1024;
+
     private IOUtils() {
         throw new UnsupportedOperationException();
     }
@@ -32,6 +34,50 @@ public final class IOUtils {
         return writer instanceof BufferedWriter
             ? (BufferedWriter) writer
             : new BufferedWriter(writer);
+    }
+
+    public static long copy(InputStream input, OutputStream output) throws IOException {
+        Check.notNull(input);
+        Check.notNull(output);
+
+        byte[] buffer = new byte[BufferSize];
+        long count = 0;
+        while (true) {
+            int read = input.read(buffer);
+            if (read < 0) {
+                return count;
+            }
+            output.write(buffer, 0, read);
+            count += read;
+        }
+    }
+
+    public static long copy(Reader reader, Writer writer) throws IOException {
+        Check.notNull(reader);
+        Check.notNull(writer);
+
+        char[] buffer = new char[BufferSize];
+        long count = 0;
+        while (true) {
+            int read = reader.read(buffer);
+            if (read < 0) {
+                return count;
+            }
+            writer.write(buffer, 0, read);
+            count += read;
+        }
+    }
+
+    public static byte[] readAllBytes(InputStream input) throws IOException {
+        ByteArrayOutputStream output = new ByteArrayOutputStream(BufferSize);
+        copy(input, output);
+        return output.toByteArray();
+    }
+
+    public static String readAllText(Reader reader) throws IOException {
+        StringWriter writer = new StringWriter(BufferSize);
+        copy(reader, writer);
+        return writer.toString();
     }
 
 }
