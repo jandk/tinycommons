0c03c71504876f1b214cc2db35f00b78d7d182bf
Jan De Kock <jan@twofold.be>
2021-01-03 11:03:02 +0100

    Rework some tests

--- src/test/java/be/twofold/common/CheckTest.java
+++ src/test/java/be/twofold/common/CheckTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.*;
 
 import static org.assertj.core.api.Assertions.*;
 
-@SuppressWarnings({"ConstantConditions", "ObviousNullCheck"})
+@SuppressWarnings({"ConstantConditions"})
 public class CheckTest {
 
     @Test
@@ -13,99 +13,73 @@ public class CheckTest {
     }
 
     @Test
-    public void checkNotNullShouldPassWithTrueExpression() {
-        Check.notNull(new Object());
+    public void testCheckNotNull() {
+        Object object = new Object();
+
+        assertThat(Check.notNull(object)).isSameAs(object);
+        assertThatNullPointerException()
+            .isThrownBy(() -> Check.notNull(null));
+
+        assertThat(Check.notNull(object, "object")).isSameAs(object);
+        assertThatNullPointerException()
+            .isThrownBy(() -> Check.notNull(null, "object"))
+            .withMessage("object");
+
+        assertThat(Check.notNull(object, "%s is null", "object")).isSameAs(object);
+        assertThatNullPointerException()
+            .isThrownBy(() -> Check.notNull(null, "%s is null", "object"))
+            .withMessage("object is null");
     }
 
     @Test
-    public void checkNotNullReturnsOriginalObject() {
-        Object value = new Object();
-        assertThat(Check.notNull(value))
-            .isSameAs(value);
+    public void testCheckArgument() {
+        assertThatNoException()
+            .isThrownBy(() -> Check.argument(true));
+        assertThatIllegalArgumentException()
+            .isThrownBy(() -> Check.argument(false));
+
+        assertThatNoException()
+            .isThrownBy(() -> Check.argument(true, "failure"));
+        assertThatIllegalArgumentException()
+            .isThrownBy(() -> Check.argument(false, "failure"))
+            .withMessage("failure");
+
+        assertThatNoException()
+            .isThrownBy(() -> Check.argument(true, "failure %s", "argument"));
+        assertThatIllegalArgumentException()
+            .isThrownBy(() -> Check.argument(false, "failure %s", "argument"))
+            .withMessage("failure argument");
     }
 
     @Test
-    public void checkNotNullShouldFailWithFalseExpression() {
-        assertThatThrownBy(() -> Check.notNull(null))
-            .isInstanceOf(NullPointerException.class);
+    public void testCheckState() {
+        assertThatNoException()
+            .isThrownBy(() -> Check.state(true));
+        assertThatIllegalStateException()
+            .isThrownBy(() -> Check.state(false));
+
+        assertThatNoException()
+            .isThrownBy(() -> Check.state(true, "failure"));
+        assertThatIllegalStateException()
+            .isThrownBy(() -> Check.state(false, "failure"))
+            .withMessage("failure");
+
+        assertThatNoException()
+            .isThrownBy(() -> Check.state(true, "failure %s", "state"));
+        assertThatIllegalStateException()
+            .isThrownBy(() -> Check.state(false, "failure %s", "state"))
+            .withMessage("failure state");
     }
 
     @Test
-    public void checkNotNullShouldFailWithCorrectMessage() {
-        assertThatThrownBy(() -> Check.notNull(null, "failure"))
-            .isInstanceOf(NullPointerException.class)
-            .hasMessage("failure");
-    }
-
-    @Test
-    public void checkNotNullShouldFailWithCorrectSuppliedMessage() {
-        assertThatThrownBy(() -> Check.notNull(null, "failure %s", "argument"))
-            .isInstanceOf(NullPointerException.class)
-            .hasMessage("failure argument");
-    }
-
-
-    @Test
-    public void checkArgumentShouldPassWithTrueExpression() {
-        Check.argument(true);
-    }
-
-    @Test
-    public void checkArgumentShouldFailWithFalseExpression() {
-        assertThatThrownBy(() -> Check.argument(false))
-            .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @Test
-    public void checkArgumentShouldFailWithCorrectMessage() {
-        assertThatThrownBy(() -> Check.argument(false, "failure"))
-            .isInstanceOf(IllegalArgumentException.class)
-            .hasMessage("failure");
-    }
-
-    @Test
-    public void checkArgumentShouldFailWithCorrectSuppliedMessage() {
-        assertThatThrownBy(() -> Check.argument(false, "failure %s", "argument"))
-            .isInstanceOf(IllegalArgumentException.class)
-            .hasMessage("failure argument");
-    }
-
-
-    @Test
-    public void checkStateShouldPassWithTrueExpression() {
-        Check.state(true);
-    }
-
-    @Test
-    public void checkStateShouldFailWithFalseExpression() {
-        assertThatThrownBy(() -> Check.state(false))
-            .isInstanceOf(IllegalStateException.class);
-    }
-
-    @Test
-    public void checkStateShouldFailWithCorrectMessage() {
-        assertThatThrownBy(() -> Check.state(false, "failure %s", "argument"))
-            .isInstanceOf(IllegalStateException.class)
-            .hasMessage("failure argument");
-    }
-
-    @Test
-    public void checkStateShouldFailWithCorrectSuppliedMessage() {
-        assertThatThrownBy(() -> Check.state(false, "failure %s", "argument"))
-            .isInstanceOf(IllegalStateException.class)
-            .hasMessage("failure argument");
-    }
-
-
-    @Test
-    public void checkIndexShouldPass() {
+    public void testCheckIndexShouldPass() {
         assertThat(Check.index(0, 1)).isEqualTo(0);
         assertThat(Check.index(0, 2)).isEqualTo(0);
         assertThat(Check.index(1, 2)).isEqualTo(1);
     }
 
     @Test
-    public void checkIndexShouldFail() {
+    public void testCheckIndexShouldFail() {
         assertThatThrownBy(() -> Check.index(1, -1))
             .isInstanceOf(IndexOutOfBoundsException.class); // negative size
         assertThatThrownBy(() -> Check.index(-1, 1))
@@ -116,14 +90,14 @@ public class CheckTest {
 
 
     @Test
-    public void checkPositionShouldPass() {
+    public void testCheckPositionShouldPass() {
         assertThat(Check.position(0, 0)).isEqualTo(0);
         assertThat(Check.position(0, 1)).isEqualTo(0);
         assertThat(Check.position(1, 1)).isEqualTo(1);
     }
 
     @Test
-    public void checkPositionShouldFail() {
+    public void testCheckPositionShouldFail() {
         assertThatThrownBy(() -> Check.position(1, -1))
             .isInstanceOf(IndexOutOfBoundsException.class); // negative size
         assertThatThrownBy(() -> Check.position(-1, 1))
@@ -133,7 +107,7 @@ public class CheckTest {
     }
 
     @Test
-    public void checkPositionsShouldPass() {
+    public void testCheckPositionsShouldPass() {
         Check.positions(0, 0, 0);
         Check.positions(0, 0, 1);
         Check.positions(0, 1, 1);
@@ -141,7 +115,7 @@ public class CheckTest {
     }
 
     @Test
-    public void checkPositionsShouldFail() {
+    public void testCheckPositionsShouldFail() {
         assertThatThrownBy(() -> Check.positions(1, 1, -1))
             .isInstanceOf(IndexOutOfBoundsException.class); // negative size
         assertThatThrownBy(() -> Check.positions(-1, 1, 1))
