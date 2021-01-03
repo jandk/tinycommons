03bc2a47b21ab25e514029293ad85343e95bc4c4
Jan De Kock <jan@twofold.be>
2021-01-03 20:52:12 +0100

    Add static factory method

--- src/main/java/be/twofold/common/tuple/Pair.java
+++ src/main/java/be/twofold/common/tuple/Pair.java
@@ -2,21 +2,25 @@ package be.twofold.common.tuple;
 
 import java.util.*;
 
-public final class Pair<T1, T2> {
+public final class Pair<T, U> {
 
-    private final T1 first;
-    private final T2 second;
+    private final T first;
+    private final U second;
 
-    public Pair(T1 first, T2 second) {
+    public Pair(T first, U second) {
         this.first = first;
         this.second = second;
     }
 
-    public T1 getFirst() {
+    public static <T, U> Pair<T, U> pair(T first, U second) {
+        return new Pair<>(first, second);
+    }
+
+    public T getFirst() {
         return first;
     }
 
-    public T2 getSecond() {
+    public U getSecond() {
         return second;
     }
 
