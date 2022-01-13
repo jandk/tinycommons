e1dbb7b0156520236b08113b5ca9a57de431a22e
Jan De Kock <jan@twofold.be>
2022-01-13 17:38:37 +0100

    Update module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,3 +1,13 @@
 module tinycommons {
     requires java.base;
+
+    // Only used by the ArrayUtilsGenerator in test
+    requires java.compiler;
+
+    exports be.twofold.common;
+    exports be.twofold.common.func;
+    exports be.twofold.common.func.fi;
+    exports be.twofold.common.seq;
+    exports be.twofold.common.text;
+    exports be.twofold.common.tuple;
 }
