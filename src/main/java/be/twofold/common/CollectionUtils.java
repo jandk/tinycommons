eaa2fddedba91c9dfb38ae3ceae0beb8703df666
Jan De Kock <jan@twofold.be>
2022-01-14 01:17:17 +0100

    Add some toXXXArray methods to CollectionUtils

--- src/main/java/be/twofold/common/CollectionUtils.java
+++ src/main/java/be/twofold/common/CollectionUtils.java
@@ -28,4 +28,80 @@ public final class CollectionUtils {
         return map == null || map.isEmpty();
     }
 
+    // region toArray
+
+    public static byte[] toByteArray(Collection<? extends Number> collection) {
+        Check.notNull(collection, "collection");
+
+        int i = 0;
+        byte[] array = new byte[collection.size()];
+        for (Number number : collection) {
+            array[i++] = number.byteValue();
+        }
+
+        return array;
+    }
+
+    public static short[] toShortArray(Collection<? extends Number> collection) {
+        Check.notNull(collection, "collection");
+
+        int i = 0;
+        short[] array = new short[collection.size()];
+        for (Number number : collection) {
+            array[i++] = number.shortValue();
+        }
+
+        return array;
+    }
+
+    public static int[] toIntArray(Collection<? extends Number> collection) {
+        Check.notNull(collection, "collection");
+
+        int i = 0;
+        int[] array = new int[collection.size()];
+        for (Number number : collection) {
+            array[i++] = number.intValue();
+        }
+
+        return array;
+    }
+
+    public static long[] toLongArray(Collection<? extends Number> collection) {
+        Check.notNull(collection, "collection");
+
+        int i = 0;
+        long[] array = new long[collection.size()];
+        for (Number number : collection) {
+            array[i++] = number.longValue();
+        }
+
+        return array;
+    }
+
+    public static float[] toFloatArray(Collection<? extends Number> collection) {
+        Check.notNull(collection, "collection");
+
+        int i = 0;
+        float[] array = new float[collection.size()];
+        for (Number number : collection) {
+            array[i++] = number.floatValue();
+        }
+
+        return array;
+    }
+
+    public static double[] toDoubleArray(Collection<? extends Number> collection) {
+        Check.notNull(collection, "collection");
+
+        int i = 0;
+        double[] array = new double[collection.size()];
+        for (Number number : collection) {
+            array[i++] = number.doubleValue();
+        }
+
+        return array;
+    }
+
+    // endregion
+
 }
