675dd0e736b7024d533d71abaed5b0b1449d3984
Jan De Kock <jan@twofold.be>
2022-02-04 14:47:30 +0100

    Merge all Seq stuff

--- src/main/java/be/twofold/common/seq/Seq.java
+++ src/main/java/be/twofold/common/seq/Seq.java
@@ -15,9 +15,14 @@ import java.util.stream.*;
  *
  * @param <T> The type of elements in this Seq
  */
-public interface Seq<T> extends Iterable<T> {
+public abstract class Seq<T> implements Iterable<T> {
 
-    // region Construction
+    private static final Seq<?> Empty = seq(Collections::emptyIterator);
+
+    // Construction
+
+    private Seq() {
+    }
 
     /**
      * Creates a new empty Seq
@@ -25,108 +30,106 @@ public interface Seq<T> extends Iterable<T> {
      * @param <T> The type of the elements
      * @return The new Seq
      */
-    static <T> Seq<T> empty() {
-        return Collections::emptyIterator;
+    @SuppressWarnings("unchecked")
+    public static <T> Seq<T> empty() {
+        return (Seq<T>) Empty;
     }
 
     /**
-     * Creates a new Seq from the specified values.
+     * Creates a new Seq from the specified elements.
      *
      * @param <T> The type of the elements
      * @return The new Seq
      */
     @SafeVarargs
-    static <T> Seq<T> of(T... values) {
-        Check.notNull(values, "values");
-        if (values.length == 0) {
+    public static <T> Seq<T> of(T... elements) {
+        Check.notNull(elements, "elements is null");
+        if (elements.length == 0) {
             return empty();
         }
-        return () -> Arrays.asList(values).iterator();
+        return seq(() -> Arrays.asList(elements).iterator());
     }
 
     /**
      * Creates a new Seq from an existing iterable
      */
-    static <T> Seq<T> seq(Iterable<T> iterable) {
-        Check.notNull(iterable, "iterable");
-        return iterable::iterator;
+    public static <T> Seq<T> seq(Iterable<T> iterable) {
+        return new SeqImpl<>(iterable);
     }
 
-    static <T> Seq<T> seq(Iterator<T> iterator) {
-        Check.notNull(iterator, "iterator");
-        return ((Seq<T>) () -> iterator).once();
+    public static <T> Seq<T> seq(Iterator<T> iterator) {
+        Check.notNull(iterator, "iterator is null");
+        return seq(() -> iterator).once();
     }
 
-    static <T> Seq<T> seq(Stream<T> stream) {
-        return ((Seq<T>) stream::iterator).once();
+    public static <T> Seq<T> seq(Stream<T> stream) {
+        return seq(stream::iterator).once();
     }
 
-    // endregion
+    // Intermediate Operations
 
-    // region Intermediate
-
-    default Seq<T> distinct() {
+    public final Seq<T> distinct() {
         Set<T> seen = new HashSet<>();
         return filter(seen::add);
     }
 
-    default Seq<T> drop(int count) {
+    public final Seq<T> drop(int count) {
         Check.argument(count >= 0, "Negative count");
         if (count == 0) {
             return this;
         }
-        return () -> new ItrDrop<>(iterator(), count);
+        return seq(() -> new DropIterator<>(iterator(), count));
     }
 
-    default Seq<T> filter(Predicate<? super T> predicate) {
+    public final Seq<T> filter(Predicate<? super T> predicate) {
         Check.notNull(predicate, "predicate");
 
-        return () -> new ItrFilter<>(iterator(), predicate);
+        return seq(() -> new FilterIterator<>(iterator(), predicate));
     }
 
-    default Seq<T> filterIndexed(BiPredicate<Integer, ? super T> predicate) {
+    public final Seq<T> filterIndexed(BiPredicate<Integer, ? super T> predicate) {
         Check.notNull(predicate, "predicate");
 
         AtomicInteger index = new AtomicInteger();
         return filter(t -> predicate.test(index.getAndIncrement(), t));
     }
 
-    default <R> Seq<R> flatMap(Function<? super T, ? extends Iterable<? extends R>> mapper) {
+    public final <R> Seq<R> flatMap(Function<? super T, ? extends Iterable<? extends R>> mapper) {
         Check.notNull(mapper, "mapper");
 
-        return () -> new ItrFlatMap<>(iterator(), mapper);
+        return seq(() -> new FlatMapIterator<>(iterator(), mapper));
     }
 
-    default <R> Seq<R> flatMapIndexed(BiFunction<Integer, ? super T, ? extends Iterable<? extends R>> mapper) {
+    public final <R> Seq<R> flatMapIndexed(BiFunction<Integer, ? super T, ? extends Iterable<? extends R>> mapper) {
         Check.notNull(mapper, "mapper");
 
         AtomicInteger index = new AtomicInteger();
         return flatMap(t -> mapper.apply(index.getAndIncrement(), t));
     }
 
-    default Seq<Pair<Integer, T>> indexed() {
+    public final Seq<Pair<Integer, T>> indexed() {
         AtomicInteger index = new AtomicInteger();
         return map(t -> Pair.of(index.getAndIncrement(), t));
     }
 
-    default <R> Seq<R> map(Function<? super T, ? extends R> mapper) {
+    public final <R> Seq<R> map(Function<? super T, ? extends R> mapper) {
         Check.notNull(mapper, "mapper");
 
-        return () -> new ItrMap<>(iterator(), mapper);
+        return seq(() -> new MapIterator<>(iterator(), mapper));
     }
 
-    default <R> Seq<R> mapIndexed(BiFunction<Integer, ? super T, ? extends R> mapper) {
+    public final <R> Seq<R> mapIndexed(BiFunction<Integer, ? super T, ? extends R> mapper) {
         Check.notNull(mapper, "mapper");
 
         AtomicInteger index = new AtomicInteger();
         return map(t -> mapper.apply(index.getAndIncrement(), t));
     }
 
-    default Seq<T> once() {
-        return this instanceof SeqOnce ? this : new SeqOnce<>(this);
+    public final Seq<T> once() {
+        return this instanceof OnceSeq ? this : new OnceSeq<>(this);
     }
 
-    default Seq<T> onEach(Consumer<? super T> action) {
+    public final Seq<T> onEach(Consumer<? super T> action) {
         Check.notNull(action, "action");
 
         return map(element -> {
@@ -135,7 +138,7 @@ public interface Seq<T> extends Iterable<T> {
         });
     }
 
-    default Seq<T> onEachIndexed(BiConsumer<Integer, ? super T> action) {
+    public final Seq<T> onEachIndexed(BiConsumer<Integer, ? super T> action) {
         Check.notNull(action, "action");
 
         AtomicInteger index = new AtomicInteger();
@@ -146,33 +149,31 @@ public interface Seq<T> extends Iterable<T> {
     }
 
     @SuppressWarnings("unchecked")
-    default Seq<T> sorted() {
+    public final Seq<T> sorted() {
         return sorted((Comparator<? super T>) Comparator.naturalOrder());
     }
 
-    default Seq<T> sorted(Comparator<? super T> comparator) {
+    public final Seq<T> sorted(Comparator<? super T> comparator) {
         Check.notNull(comparator, "comparator");
 
-        return () -> {
+        return seq(() -> {
             List<T> list = toList();
             list.sort(comparator);
             return list.iterator();
-        };
+        });
     }
 
-    default Seq<T> take(int count) {
+    public final Seq<T> take(int count) {
         Check.argument(count >= 0, "Negative count");
         if (count == 0) {
             return empty();
         }
-        return () -> new ItrTake<>(iterator(), count);
+        return seq(() -> new TakeIterator<>(iterator(), count));
     }
 
-    // endregion
+    // Terminal Operations
 
-    // region Terminal
-
-    default boolean all(Predicate<? super T> predicate) {
+    public final boolean all(Predicate<? super T> predicate) {
         Check.notNull(predicate, "predicate");
 
         for (T element : this) {
@@ -183,15 +184,15 @@ public interface Seq<T> extends Iterable<T> {
         return true;
     }
 
-    default boolean any() {
+    public final boolean any() {
         return iterator().hasNext();
     }
 
-    default boolean any(Predicate<? super T> predicate) {
+    public final boolean any(Predicate<? super T> predicate) {
         return filter(predicate).any();
     }
 
-    default int count() {
+    public final int count() {
         int count = 0;
         for (T ignored : this) {
             count++;
@@ -199,19 +200,19 @@ public interface Seq<T> extends Iterable<T> {
         return count;
     }
 
-    default int count(Predicate<? super T> predicate) {
+    public final int count(Predicate<? super T> predicate) {
         return filter(predicate).count();
     }
 
-    default T first() {
+    public final T first() {
         return nonEmpty(iterator()).next();
     }
 
-    default T first(Predicate<? super T> predicate) {
+    public final T first(Predicate<? super T> predicate) {
         return filter(predicate).first();
     }
 
-    default void forEach(Consumer<? super T> consumer) {
+    public final void forEach(Consumer<? super T> consumer) {
         Check.notNull(consumer, "consumer");
 
         for (T element : this) {
@@ -219,42 +220,40 @@ public interface Seq<T> extends Iterable<T> {
         }
     }
 
-    default void forEachIndexed(BiConsumer<Integer, ? super T> consumer) {
+    public final void forEachIndexed(BiConsumer<Integer, ? super T> consumer) {
         Check.notNull(consumer, "consumer");
 
         AtomicInteger index = new AtomicInteger();
         forEach(t -> consumer.accept(index.getAndIncrement(), t));
     }
 
-    default <R> R fold(R initial, BiFunction<R, ? super T, ? extends R> operation) {
+    public final <R> R fold(R initial, BiFunction<R, ? super T, ? extends R> operation) {
         return fold(iterator(), initial, operation);
     }
 
-    default T last() {
+    public final T last() {
         return reduce((first, second) -> second);
     }
 
-    default T last(Predicate<? super T> predicate) {
+    public final T last(Predicate<? super T> predicate) {
         return filter(predicate).last();
     }
 
-    default boolean none() {
+    public final boolean none() {
         return !iterator().hasNext();
     }
 
-    default boolean none(Predicate<? super T> predicate) {
+    public final boolean none(Predicate<? super T> predicate) {
         return filter(predicate).none();
     }
 
-    default T reduce(BinaryOperator<T> operation) {
+    public final T reduce(BinaryOperator<T> operation) {
         return reduce(iterator(), operation);
     }
 
-    // endregion
+    // Collectors
 
-    // region Collectors
-
-    default <C extends Collection<? super T>> C toCollection(C destination) {
+    public final <C extends Collection<? super T>> C toCollection(C destination) {
         Check.notNull(destination, "destination");
 
         for (T element : this) {
@@ -263,23 +262,23 @@ public interface Seq<T> extends Iterable<T> {
         return destination;
     }
 
-    default List<T> toList() {
+    public final List<T> toList() {
         return toCollection(new ArrayList<>());
     }
 
-    default Set<T> toSet() {
+    public final Set<T> toSet() {
         return toCollection(new HashSet<>());
     }
 
-    default List<T> toUnmodifiableList() {
+    public final List<T> toUnmodifiableList() {
         return List.copyOf(toList());
     }
 
-    default Set<T> toUnmodifiableSet() {
+    public final Set<T> toUnmodifiableSet() {
         return Set.copyOf(toList());
     }
 
-    default <K, V, M extends Map<K, V>> M toMap(
+    public final <K, V, M extends Map<K, V>> M toMap(
         Function<? super T, ? extends K> keyMapper,
         Function<? super T, ? extends V> valueMapper,
         M destination
@@ -300,7 +299,7 @@ public interface Seq<T> extends Iterable<T> {
         return destination;
     }
 
-    // endregion
+    // Helpers
 
     private static <E, R> R fold(Iterator<E> iterator, R initial, BiFunction<R, ? super E, ? extends R> operation) {
         Check.notNull(operation, "operation");
@@ -321,6 +320,176 @@ public interface Seq<T> extends Iterable<T> {
             throw new NoSuchElementException();
         }
         return iterator;
+    }
+
+    // Implementations
+
+    private static final class SeqImpl<T> extends Seq<T> {
+        private final Iterable<T> iterable;
+
+        private SeqImpl(Iterable<T> iterable) {
+            this.iterable = Check.notNull(iterable, "iterable is null");
+        }
+
+        @Override
+        public Iterator<T> iterator() {
+            return iterable.iterator();
+        }
+    }
+
+    private static final class OnceSeq<T> extends Seq<T> {
+        private final AtomicReference<Seq<T>> reference;
+
+        private OnceSeq(Seq<T> seq) {
+            reference = new AtomicReference<>(seq);
+        }
+
+        @Override
+        public Iterator<T> iterator() {
+            Seq<T> seq = reference.getAndSet(null);
+            if (seq == null) {
+                throw new IllegalStateException("Sequence can only be iterated once");
+            }
+            return seq.iterator();
+        }
+    }
+
+    private static final class DropIterator<E> implements Iterator<E> {
+        private final Iterator<E> iterator;
+        private int count;
+
+        private DropIterator(Iterator<E> iterator, int count) {
+            this.iterator = Check.notNull(iterator, "iterator is null");
+            this.count = count;
+        }
+
+        @Override
+        public boolean hasNext() {
+            while (count > 0 && iterator.hasNext()) {
+                iterator.next();
+                count--;
+            }
+            return iterator.hasNext();
+        }
+
+        @Override
+        public E next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            return iterator.next();
+        }
+    }
+
+    private static final class FilterIterator<E> implements Iterator<E> {
+        private final Iterator<E> iterator;
+        private final Predicate<? super E> predicate;
+        private boolean hasNext;
+        private E next;
+
+        private FilterIterator(Iterator<E> iterator, Predicate<? super E> predicate) {
+            this.iterator = Check.notNull(iterator, "iterator is null");
+            this.predicate = Check.notNull(predicate, "predicate is null");
+        }
+
+        @Override
+        public boolean hasNext() {
+            while (!hasNext && iterator.hasNext()) {
+                E next = iterator.next();
+                if (predicate.test(next)) {
+                    hasNext = true;
+                    this.next = next;
+                }
+            }
+            return hasNext;
+        }
+
+        @Override
+        public E next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            hasNext = false;
+            E result = next;
+            next = null;
+            return result;
+        }
+    }
+
+    private static final class FlatMapIterator<E, R> implements Iterator<R> {
+        private final Iterator<E> iterator;
+        private final Function<? super E, ? extends Iterable<? extends R>> mapper;
+        private Iterator<? extends R> itemIterator = Collections.emptyIterator();
+
+        private FlatMapIterator(Iterator<E> iterator, Function<? super E, ? extends Iterable<? extends R>> mapper) {
+            this.iterator = Check.notNull(iterator, "iterator is null");
+            this.mapper = Check.notNull(mapper, "mapper is null");
+        }
+
+        @Override
+        public boolean hasNext() {
+            while (true) {
+                if (itemIterator.hasNext()) {
+                    return true;
+                }
+                if (!iterator.hasNext()) {
+                    return false;
+                }
+                itemIterator = mapper.apply(iterator.next()).iterator();
+            }
+        }
+
+        @Override
+        public R next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            return itemIterator.next();
+        }
+    }
+
+    private static final class MapIterator<E, R> implements Iterator<R> {
+        private final Iterator<E> iterator;
+        private final Function<? super E, ? extends R> mapper;
+
+        private MapIterator(Iterator<E> iterator, Function<? super E, ? extends R> mapper) {
+            this.iterator = Check.notNull(iterator, "iterator is null");
+            this.mapper = Check.notNull(mapper, "mapper is null");
+        }
+
+        @Override
+        public boolean hasNext() {
+            return iterator.hasNext();
+        }
+
+        @Override
+        public R next() {
+            return mapper.apply(iterator.next());
+        }
+    }
+
+    private static final class TakeIterator<E> implements Iterator<E> {
+        private final Iterator<E> iterator;
+        private int count;
+
+        private TakeIterator(Iterator<E> iterator, int count) {
+            this.iterator = Check.notNull(iterator, "iterator is null");
+            this.count = count;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return count > 0 && iterator.hasNext();
+        }
+
+        @Override
+        public E next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            count--;
+            return iterator.next();
+        }
     }
 
 }
